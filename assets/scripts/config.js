e3b12f2d3c160d432341aa9d681a5d8fb5249744
jbeltrami <joao_beltrami@hotmail.com>
2017-05-04 14:38:17 -0400

    Adds CSS and HTML, config the URLs to ping API
    Starts HTML and CSS.
    Sets the URLs to ping correct API for production and development.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://tranquil-stream-75403.herokuapp.com/',
+    development: 'http://localhost:4741'
   }
 }
 
