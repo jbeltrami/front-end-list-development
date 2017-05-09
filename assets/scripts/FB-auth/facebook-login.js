5607f5efacc14bfd5f893cfa8aea1b794c5ecfee
jbeltrami <joao_beltrami@hotmail.com>
2017-05-09 13:55:45 -0400

    Adds non-functional facebook button
    Button is going to trigger actions to facebook API.

--- assets/scripts/FB-auth/facebook-login.js
+++ assets/scripts/FB-auth/facebook-login.js
@@ -1,1 +1,17 @@
-
+  // window.fbAsyncInit = function () {
+  //   FB.init({
+  //     appId: '301249823663546',
+  //     xfbml: true,
+  //     version: 'v2.9'
+  //   })
+  //   FB.AppEvents.logPageView()
+  // }(function (d, s, id) {
+  //   var js, fjs = d.getElementsByTagName(s)[0]
+  //   if (d.getElementById(id)) {
+  //     return
+  //   }
+  //   js = d.createElement(s)
+  //   js.id = id
+  //   js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=301249823663546'
+  //   fjs.parentNode.insertBefore(js, fjs)
+  // }(document, 'script', 'facebook-jssdk'))
